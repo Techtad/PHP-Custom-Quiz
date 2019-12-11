e19b948551966a881daab0369d9be45b8bf73f7d
Tadeusz Kantor <tkantor@inf19.local>
2019-12-11 13:08:25 +0100

    .

--- www/php/admin/deleteuser.php
+++ www/php/admin/deleteuser.php
@@ -1,16 +1,20 @@
 <?php
-if(!require($_SERVER["DOCUMENT_ROOT"] . "/php/auth/isadmin.php")) {header("Location: /"); return;}
+if (!require($_SERVER["DOCUMENT_ROOT"] . "/php/auth/isadmin.php")) {
+    header("Location: /");
+    return;
+}
 
-if(isset($_POST["username"])) {
+if (isset($_POST["username"])) {
     $user = $_POST["username"];
+    if ($user == "admin") die("Nie można usunąć admina.");
 
     require_once $_SERVER["DOCUMENT_ROOT"] . "/php/settings.php";
-    if(!$conn = db_connect()) die("Nie udało się połączyć z bazą danych.");
-    if($conn->connect_error) die("Nie udało się połączyć z bazą danych.");
+    if (!$conn = db_connect()) die("Nie udało się połączyć z bazą danych.");
+    if ($conn->connect_error) die("Nie udało się połączyć z bazą danych.");
 
     $conn->begin_transaction();
-        $conn->query(sprintf("DELETE FROM `accounts` WHERE `username` LIKE '%s'", $user));
-        $conn->query(sprintf("DELETE FROM `scores` WHERE `user` LIKE '%s'", $user));
+    $conn->query(sprintf("DELETE FROM `accounts` WHERE `username` LIKE '%s'", $user));
+    $conn->query(sprintf("DELETE FROM `scores` WHERE `user` LIKE '%s'", $user));
     $conn->commit();
 
     $conn->close();
